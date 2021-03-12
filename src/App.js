dd8d86506567fd1d1c5a000435d738d3cd3dedb5
IzcoGLeon <Izco.g.leon@outlook.com>
2021-03-12 00:28:36 -0600

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'; //Para hacer uso de funcionalidades importamos componentes
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
+//El componente Route toma una serie de  argumentos, siendo principales 'exact'(con valor true por default, solo se coincidirá para renderizado si el path coincide con el location.pathname, o path, de manera precisa), 'path' (el string que será equivalente al path mismo del lugar en el que nos encontraremos), 'component' (el componente que queremos renderizar)
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
 
+//Switch es otro componente que renderiza el primer hijo <Route> o <Redirect> que coincide con la localización. Switch es unico ya que renderiza una ruta (Route) de manera exclusiva, al momento que Switch ve que algo coincide con path (de cada ruta) entonces solo renderizará esa Route.
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'; //Ya que instalamos react-router-dom, para comenzar a utilizarlo debemos importar el componente BrowserRouter en index.js. Este es un componente que usaremos para envolver nuestra aplicación 
+
 import './index.css';
 import App from './App';
 
-
+//Lo que este componente hace es que proporciona a la aplicación (envuelta entre este componente) toda la funcionalidad de routing de la que esta libreria dispone 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
